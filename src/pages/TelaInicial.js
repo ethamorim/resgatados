cace31466901d27dc03a67f6e4951007e983e1f6
YTALO DOS SANTOS AMORIM <2020313187@IFAL.LOCAL>
2022-11-01 15:08:36 -0300

    finalizando estilo de tela inicial

--- src/pages/TelaInicial.js
+++ src/pages/TelaInicial.js
@@ -1,19 +1,42 @@
 import React from "react";
 
+import Botao from "../components/Botao";
+
 import LogoResgatados from '../assets/images/logo.svg'
 import TituloResgatados from '../assets/images/titulo-resgatados.svg';
 import DivisorResgatados from '../assets/images/divisor.svg';
+import PetsIniciar from '../assets/images/pets_iniciar.svg';
+import PataOutline from '../assets/images/pata_outline.svg';
 
 
 function TelaInicial() {
 
   return (
-    <main>
-      <div className="logo-conteiner">
+    <main className="tela-inicial">
+      <section className="logo-conteiner">
         <img className="logo" src={LogoResgatados} alt="" />
         <img className="titulo" src={TituloResgatados} alt="" />
         <img className="divisor" src={DivisorResgatados} alt="" />
-      </div>
+        <p className="slogan">Juntando você e sua alma gêmea de até quatro patas!</p>
+      </section>
+
+      <section className="acao-conteiner">
+        <Botao className="entrar" titulo="Entrar" />
+        <Botao className="cadastrar" titulo="Cadastrar" />
+
+        <a className="esqueceu-senha">Esqueceu a senha?</a>
+      </section>
+
+      <section className="pets-iniciar">
+        <img src={PetsIniciar} alt="" />
+      </section>
+
+      <article className="tela-inicial__background">
+        <img className="pata_1" src={PataOutline} />
+        <img className="pata_2" src={PataOutline} />
+        <img className="pata_3" src={PataOutline} />
+        <img className="pata_4" src={PataOutline} />
+      </article>
     </main>
   );
 }
