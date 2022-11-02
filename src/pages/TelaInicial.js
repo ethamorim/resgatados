ef94d2f3b75f292f7450bc79aa9295d0e5dba9ec
Ethan <ysa4@aluno.ifal.edu.br>
2022-11-02 18:26:50 -0300

    tentando resolver problema de overflow

--- src/pages/TelaInicial.js
+++ src/pages/TelaInicial.js
@@ -3,7 +3,7 @@ import React from "react";
 import Botao from "../components/Botao";
 
 import LogoResgatados from '../assets/images/logo.svg'
-import TituloResgatados from '../assets/images/titulo-resgatados.svg';
+import TituloResgatados from '../assets/images/titulo_resgatados.svg';
 import DivisorResgatados from '../assets/images/divisor.svg';
 import PetsIniciar from '../assets/images/pets_iniciar.svg';
 import PataOutline from '../assets/images/pata_outline.svg';
@@ -24,7 +24,7 @@ function TelaInicial() {
         <Botao className="entrar" titulo="Entrar" />
         <Botao className="cadastrar" titulo="Cadastrar" />
 
-        <a className="esqueceu-senha">Esqueceu a senha?</a>
+        <a className="esqueceu-senha" href="/">Esqueceu a senha?</a>
       </section>
 
       <section className="pets-iniciar">
@@ -32,10 +32,10 @@ function TelaInicial() {
       </section>
 
       <article className="tela-inicial__background">
-        <img className="pata_1" src={PataOutline} />
-        <img className="pata_2" src={PataOutline} />
-        <img className="pata_3" src={PataOutline} />
-        <img className="pata_4" src={PataOutline} />
+        <img className="pata_1" src={PataOutline} alt="" />
+        <img className="pata_2" src={PataOutline} alt="" />
+        <img className="pata_3" src={PataOutline} alt="" />
+        <img className="pata_4" src={PataOutline} alt="" />
       </article>
     </main>
   );
